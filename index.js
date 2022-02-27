b69d0a3dbf9f8e7d92f0fd77ca23fc072387c028
Arthur-codeX <daniel.karanja239@gmail.com>
2022-02-27 20:33:39 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
-
 // Write your code here
+let num1 = 2;
+let num2 = 31;
+let num3 = 24;
+let num4 = 5;
+
+let multiply = num1 * num2;
+
+let random = Math.floor(Math.random() * (10000 - 0)) + 0;
+
+let mod = 24 % 5;
+
+let max = 20;
